800157decbfb8f4974878b950d479af02dbcc82d
Mateusz Pogoda <matimeusz123@gmail.com>
2023-10-09 22:44:08 +0200

    <33

---
+++ app.js
@@ -0,0 +1,67 @@
+const url = `http://localhost:3000`
+
+async function wyswietl(){
+    console.log("wyswietl")
+    const data = await fetch(`${url}/select_all`)
+    const json = await data.json()
+    
+    console.log(json)
+    document.getElementById("result").innerHTML = ""
+
+    for(let i = 0; i <= json.length - 1; i++){
+        const div = document.createElement("div")
+        div.classList.add("div")
+        document.getElementById("result").appendChild(div)
+
+        const przedmiot = document.createElement("h1")
+        przedmiot.innerHTML = json[i].przedmiot
+        div.appendChild(przedmiot)
+
+        const ocena = document.createElement("p")
+        ocena.innerHTML = json[i].ocena
+        div.appendChild(ocena)
+    }
+}
+wyswietl()
+
+
+
+async function insert_into(){
+    const przedmiot = document.getElementById("przedmiot")
+    const ocena = document.getElementById("ocena")
+    console.log("insert_into")
+    const data = await fetch(`${url}/add/${przedmiot.value}/${ocena.value}`)
+    const json = await data.json()
+    console.log(json)
+    await wyswietl()
+}
+
+async function sort(){
+    const radios = document.getElementsByName("min_max")
+    console.log(radios)
+    var value = ""
+    radios.forEach((element)=>{
+        if(element.checked === true){
+            value = element.value
+        }
+    })
+    const data = await fetch(`${url}/min_max/${value}`)
+    const json = await data.json()
+    
+    console.log(json)
+    document.getElementById("result").innerHTML = ""
+
+    for(let i = 0; i <= json.length - 1; i++){
+        const div = document.createElement("div")
+        div.classList.add("div")
+        document.getElementById("result").appendChild(div)
+
+        const przedmiot = document.createElement("h1")
+        przedmiot.innerHTML = json[i].przedmiot
+        div.appendChild(przedmiot)
+
+        const ocena = document.createElement("p")
+        ocena.innerHTML = json[i].ocena
+        div.appendChild(ocena)
+    }
+}
---
+++ main.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const cors = require("cors")
+const mysql = require("mysql")
+const port = 3000
+const app = express()
+app.use(cors())
+
+var con = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"nwm"
+})
+con.connect((err)=>{
+    if(err) console.log(err)
+    else console.log("connected :D")
+})
+app.get("/add/:przedmiot/:ocena",(req,res)=>{
+    const przedmiot = req.params.przedmiot
+    const ocena = req.params.ocena
+
+    const sql = `INSERT INTO tabela (przedmiot, ocena) VALUES ('${przedmiot}', '${ocena}')`
+
+    con.query(sql,(err,result)=>{
+        if(err) console.log(err)
+        else res.send({dodano:true})
+    })
+})
+app.get("/select_all",(req,res)=>{
+    const sql = `SELECT * FROM tabela`
+
+    con.query(sql,(err,result)=>{
+        if(err) console.log(err)
+        else res.send(result)
+    })
+})
+
+app.listen(port)
